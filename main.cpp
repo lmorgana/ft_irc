edbdc29f3e6a777eda12241e8f2777d3695e2f81
Lomas Morgana <lmorgana@et-c4.kzn.21-school.ru>
2022-03-17 19:04:37 +0300

    with main

--- main.cpp
+++ main.cpp
@@ -24,18 +24,18 @@ int main(int argc, char** argv)
 {
 	if (argc != 3)
 	{
-		std::cout << "Wrong number of arguments";
+		std::cout << "Wrong number of arguments" << std::endl;
 		return (1);
 	}
 	if (!is_digit(argv[1]))
 	{
-		std::cout << "Port must be number";
+		std::cout << "Port must be number" << std::endl;
 		return (1);
 	}
 	int port = atoi(argv[1]);
 	if (port <= MIN_PORT || port >= MAX_PORT)
 	{
-		std::cout << "Wrong port number";
+		std::cout << "Wrong port number"<< std::endl;
 		return (1);
 	}
 	EventSelector *selector = new EventSelector;
